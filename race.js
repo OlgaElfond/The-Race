65eb4c544ff712a59ab3654be693acf07c0f4a8c
OlgaElfond <ol4iki@gmail.com>
2023-03-04 22:10:06 +0200

    running icons

---
+++ race.js
@@ -0,0 +1,94 @@
+const racers = [
+  {
+    name: "malfoy",
+    raceNumber: "1",
+    speed: 13,
+    raceColor: "red",
+    icon: "./icons8-draco-malfoy-48.png",
+  },
+  {
+    name: "dobby",
+    raceNumber: "2",
+    speed: 34,
+    raceColor: "green",
+    icon: "./icons8-dobby-48.png",
+  },
+  {
+    name: "hermione",
+    raceNumber: "3",
+    speed: 10,
+    raceColor: "red",
+    icon: "./icons8-hermione-48.png",
+  },
+  {
+    name: "dumbledore",
+    speed: 20,
+    raceNumber: "4",
+    raceColor: "red",
+    icon: "./icons8-albus-dumbledore-48.png",
+  },
+];
+
+function buildTheRace(racers) {
+  racers.forEach((element) => {
+    let newDiv = document.createElement("div");
+    newDiv.className = `race${element.raceNumber}`;
+    newDiv.style.background = element.raceColor;
+    newDiv.addEventListener("click", stopSingle);
+
+    element.id = `runner${element.raceNumber}`;
+
+    newDiv.append(createImg(element));
+    document.body.appendChild(newDiv);
+
+    let intervalId = setInterval(() => {
+      moveIcon(element);
+    }, element.speed);
+
+    element.intervalId = intervalId;
+  });
+}
+
+function createImg(racer) {
+  let img = document.createElement("img");
+  img.src = racer.icon;
+  img.height = 50;
+  img.width = 50;
+  img.id = racer.id;
+  img.left = 0;
+  return img;
+}
+
+function announceWinner(winner) {
+  let theWinner = document.createElement("p");
+  theWinner.textContent = `the winner is ${winner}!`;
+  document.body.appendChild(theWinner);
+}
+
+function moveIcon(element) {
+  let img = document.querySelector(`#${element.id}`);
+
+  if (img.offsetLeft >= img.parentElement.clientWidth - img.clientWidth) {
+    announceWinner(element.name);
+    stopAll();
+    console.log(racers);
+  } else {
+    img.style.left = img.offsetLeft + 10 + "px";
+  }
+}
+
+function stopAll() {
+  racers.forEach((element) => {
+    clearInterval(element.intervalId);
+  });
+}
+
+function stopSingle(event) {
+  let stopRacer = event.target.className;
+  console.log(stopRacer);
+  ///racers.find((stopRacer) => );
+  console.log(event.target);
+  //clearInterval(element.intervalId);
+}
+
+buildTheRace(racers);
